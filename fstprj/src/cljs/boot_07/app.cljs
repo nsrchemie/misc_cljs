c61eee8d9baac33593608cb4bcecd5c2c2f394df
nsrchemie <nsrchemie@gmail.com>
2017-10-28 00:35:21 -0400

    values adjust when changed in repl)

--- fstprj/src/cljs/boot_07/app.cljs
+++ fstprj/src/cljs/boot_07/app.cljs
@@ -4,6 +4,8 @@
 (defn c-to-f [myc]
 (+ (* myc 1.8) 32))
 
+(defn f-to-c [myf]
+(/ (- myf 32) 1.8))
 (defn temp []
  [:div
  [:h3 "Temperature Converter"]
@@ -16,12 +18,21 @@
 (defn celsius []
  [:div
  [:label "celsius"]
- [:input {:value @celsius-v } ]])
+ [:input {
+  :value @celsius-v
+  :on-change
+      #(reset! celsius-v (-> % .target .-value))
+ }]])
 
  (defn fahrenheit []
   [:div
   [:label "fahrenheit"]
-  [:input {:value (c-to-f @celsius-v)}]])
+  [:input {
+      :value (c-to-f @celsius-v)
+      :on-change
+        #(reset! celsius-v (f-to-c
+        	(-> % .target .-value)))
+          }]])
 
 (defn init []
  (reagent/render-component [temp]
